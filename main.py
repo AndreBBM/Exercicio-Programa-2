f6c6700efb8d52eef25edb4de3922c80a6a9beeb
AndreBBM <81170314+AndreBBM@users.noreply.github.com>
2021-04-27 15:13:54 -0300

    Remoção da 2a "extrai-naipe" e do 3º "if" da
    Remoção da 2a "extrai-naipe" e do 3º "if" da lista_movimentos_possiveis.
    Pelo que notei esse 3º if faz a mesma coisa que o 4º e funcionou no servidor de teste. Mas seria bom se você pudesse checar também, Gi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 def cria_baralho():             # Primeira função na página do EP2
     cartas = []
     embaralhado = []
@@ -22,16 +23,11 @@ def cria_baralho():             # Primeira função na página do EP2
         cartas.remove(embaralhado[len(embaralhado) - 1])
     return embaralhado          # Cartas são dadas assim
 
+
 def extrai_naipe(carta):        # Segunda função na página do EP2
     naipe = carta[-1]           # Selecionar a última posição = naipe
     return naipe
-# Escolher qual das duas usar
-def extrai_naipe(carta):
-     if len(carta) == 3:
-         naipe = carta[2]
-     else:
-         naipe = carta[1]
-     return naipe
+
 
 def extrai_valor(carta):        # Terceira função na página do EP2
     if len(carta) == 2:         # Se for um número de um algarismo, pegar somente a primeira posição
@@ -39,19 +35,19 @@ def extrai_valor(carta):        # Terceira função na página do EP2
     if len(carta) == 3:         # Se for um número de dois algarismos, pegar as duas primeiras posições
         return carta[0:2]
 
+
 def lista_movimentos_possiveis(b,i):  # Quarta função na página do EP2  -> b = baralho; i = índice
     mov_pos = []                      # Lista de movimentos possíveis
-    if i == 0:                      
+    if i == 0:
         return mov_pos
-    if i < len(b) and i > 0 and extrai_naipe(b[i]) == extrai_naipe(b[i-1]) or extrai_valor(b[i]) == extrai_valor(b[i-1]):
+    if len(b) > i > 0 and extrai_naipe(b[i]) == extrai_naipe(b[i - 1]) or extrai_valor(b[i]) == extrai_valor(b[i - 1]):
         mov_pos.append(1)
-    if i == 2 and extrai_naipe(b[i]) != extrai_naipe(b[i-1]) and extrai_valor(b[i]) != extrai_valor(b[i-1]):
-        return mov_pos
     if (i - 3) < 0:
         return mov_pos
-    if i < len(b) and i > 2 and extrai_naipe(b[i]) == extrai_naipe(b[i-3]) or extrai_valor(b[i]) == extrai_valor(b[i-3]):
+    if len(b) > i > 2 and extrai_naipe(b[i]) == extrai_naipe(b[i - 3]) or extrai_valor(b[i]) == extrai_valor(b[i - 3]):
         mov_pos.append(3)
     return mov_pos
+
 
 def empilha(baralho, i, f):     # Quinta função na página do EP2
     if (i - f) == 1:            # Se for trocar com a anterior, simplismente apagar a anterior (o destino)
